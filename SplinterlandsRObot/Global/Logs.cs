c57bf70a8d4c1ffcc96514632ee19a682267fc9c
Alinubu <studio@alinbadea.ro>
2022-01-18 10:31:07 +0200

    fixed broken code in logs

--- SplinterlandsRObot/Global/Logs.cs
+++ SplinterlandsRObot/Global/Logs.cs
@@ -51,7 +51,7 @@ namespace SplinterlandsRObot
         public static void OutputStat()
         {
             var table = new ConsoleTable(CONSOLE_TABLE_HEADER);
-            InstanceManager.UsersStatistics.ForEach(u => table.AddRow(u.Account, u.ECR, u.Wins, u.Draws, u.Losses, Math.Round((Convert.ToDouble(u.Wins) / ((u.Wins + u.Draws + u.Losses) == 0 ? Convert.ToDouble(1) : Convert.ToDouble((u.Wins + u.Draws + u.Losses))) * Convert.ToDouble(100)),2).ToString() + "%", u.MatchRewards, Math.Round(Convert.ToDouble(u.TotalRewards),3), u.Rating + "[" + u.RatingChange + "]", u.CollectionPower, u.Quest)));
+            InstanceManager.UsersStatistics.ForEach(u => table.AddRow(u.Account, u.ECR, u.Wins, u.Draws, u.Losses, Math.Round((Convert.ToDouble(u.Wins) / ((u.Wins + u.Draws + u.Losses) == 0 ? Convert.ToDouble(1) : Convert.ToDouble((u.Wins + u.Draws + u.Losses))) * Convert.ToDouble(100)),2).ToString() + "%", u.MatchRewards, Math.Round(Convert.ToDouble(u.TotalRewards),3), u.Rating + "[" + u.RatingChange + "]", u.CollectionPower, u.Quest));
             table.Configure(o => o.NumberAlignment = Alignment.Right);
             table.Configure(table => table.EnableCount = false);
             lock (_lock)
